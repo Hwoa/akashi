60e51f8a94019ec47e60b97dd7281f6aabf6a4c9
Isao Abe <is.abe@cloudcircus.co.jp>
2022-06-07 18:15:39 +0900

    追加

--- src/content.js
+++ src/content.js
@@ -18,15 +18,46 @@ function main(e) {
         // tbody要素を取得する
         const element_body = document.querySelector('.c-main-table-body');
 
-        // trを朱とkする
+        // trを取得
         const rows = element_body.querySelectorAll(":scope > tr");
 
+        let paths = location.pathname.split("/");
+        let user_id = paths[3];
+        let target = paths[4];
+
+        console.log(user_id);
+        console.log(target);
+
+        for(let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+            let cols = row.querySelectorAll("td");
+            for (let j = 0; j < cols.length; j++) {
+                let col = cols[j];
+                console.log(col);
+                let name = "form[working_records_attributes][" + user_id + "][2022-06-01]";
+                const phone_plan = col.querySelector('input[type="hidden"][name*="' + name + '"]');
+                console.log(name);
+                console.log(phone_plan);
+                console.log(phone_plan.value);
+
+            }
+            break;
+        }
+
         // tr毎にエラーをチェックする
-        rows.forEach(function(element){
-            console.log(element);
-        });
+        // rows.forEach(function(row){
+        //     let cols = element_body.querySelectorAll("td");
+        //     cols.forEach(function(col){
+        //         console.log()
+        //     });
+        // });
 
 
+
+    }
+
+    // 開始時間が8:00、9:00、
+    function checkStart() {
 
     }
 }
