6c9cd5ea0002c8df0582afbda641cc2cd7ab4f25
Isao Abe <is.abe@cloudcircus.co.jp>
2022-06-09 19:06:09 +0900

    更新

--- src/content.js
+++ src/content.js
@@ -26,48 +26,62 @@ function main(e) {
         let user_id = paths[3];
         let target = paths[4];
 
-        console.log("user_id = " + user_id);
-        console.log("target_YM = " + target);
+        let today = formatDate(new Date());
 
-        let error_row = "";
+        // エラー文言格納用
+        let error_text = "";
+
+        const WORK_STATUS = ["勤務", "午前半年休", "午後半年休", "時差出勤"];
 
         // 1行ずつ処理を行う
         for(let i = 0; i < rows.length; i++) {
+
+            // 行を取得
             let row = rows[i];
+
+            // 行からセルを取得
             let cols = row.querySelectorAll("td");
 
-            console.log(row);
-            console.log("target_D = " + row.dataset.date);
-
-
+            // エラー用の行を用意する
+            let error_row = "";
 
             // 実績のtd
             let start_end = cols[1];
-            console.log(start_end)
 
+            // inputを分解して、開始時間と終了時間を取得する
             let arr_start_end = start_end.querySelectorAll('input[type="text"]');
 
-            console.log(arr_start_end);
-            if (arr_start_end.length == 0) {
+            // 長さが0の場合はまだ迎えていない日時、また当日は締まっていないため終了する
+            if (arr_start_end.length == 0 || today == row.dataset.date) {
                 break;
             }
 
-            console.log(arr_start_end[0].value);
-            console.log(arr_start_end[1].value);
-
-            error_row += checkStart(row.dataset.date, arr_start_end[0].value);
-
             // 勤務状況のtd
-            let status = cols[2];
+            let status = cols[2].innerText;
+
+            // どちらも未入力の場合は、それだけ伝えて次の行へ。
+            if (WORK_STATUS.includes(status) && (arr_start_end[0].value == "" || arr_start_end[1].value == "")) {
+                error_text += row.dataset.date + "に未入力があります。<br/>";
+                continue;
+            }
+
+            // 開始時間、終了時間、勤務ステータスを元に正常化判定を行う。
+            error_row += checkStart(row.dataset.date, arr_start_end[0].value, arr_start_end[0].value, status);
+
+            // 勤務状況を調べる。
+            error_row += checkStatus(row.dataset.date, status);
 
             // 総労働時間
-            let total_hours = cols[3];
+            // let total_hours = cols[3];
 
             // 不足時間
-            let lack_hours = cols[4];
+            let lack_hours = cols[4].innerText;
+            error_row += checkLack(row.dataset.date, lack_hours);
 
             // 休憩時間
-            let rest_hours = cols[5];
+            let rest_hours = cols[5].innerText;
+            error_row += checkRest(row.dataset.date, rest_hours, status);
+
 
             // 残業時間
             let over_hours = cols[6];
@@ -75,42 +89,84 @@ function main(e) {
             // 遅刻早退時間
             let over_early = cols[9];
 
-            console.log(error_row);
-
-
-
+            if (error_row != "") {
+                error_text += error_row;
+            }
 
         }
 
+        // 追加する部分を取得する
         let header = document.querySelector('.p-roster-header');
-
-        console.log(header);
 
         // 追加するhtmlの作成
         let add = document.createElement("div");
 
         // 特に意味はないけどid振っておく
-        add.setAttribute("id", "custom-error");
+        // add.setAttribute("id", "custom-error");
 
         // 色とpaddingを追加
         add.setAttribute("style", "padding-left: 30px; color:red;");
 
         // 作成したエラー文言をinnerHTMLに設定する
-        add.innerHTML = error_row;
+        add.innerHTML = error_text;
 
         // 名前の真下に挿入して終了
         header.appendChild(add);
 
     }
 
-    // 開始時間が8:00、9:00、
-    function checkStart(target_date, start_time) {
+    // 日付取得
+    function formatDate(dt) {
+        let y = dt.getFullYear();
+        let m = ('00' + (dt.getMonth()+1)).slice(-2);
+        let d = ('00' + dt.getDate()).slice(-2);
+        return (y + '-' + m + '-' + d);
+    }
 
-        if (start_time != "8:00" && start_time != "9:00" && start_time != "10:00") {
-            return target_date + "の開始時間が" + start_time + "です。<br/>"
-        } else {
+    // 開始時間の組み合わせを調べる
+    function checkStart(target_date, start_time, end_time, status) {
+
+        const START_ARRAY = ["8:00", "9:00", "10:00"];
+        const HOLIDAY_ARRAY = ["休日", "年休", "コロナ休暇"];
+
+        // 問題がない場合の組み合わせ
+        if (
+            START_ARRAY.includes(start_time) ||            // 開始時間が勤務で正常な場合
+            (HOLIDAY_ARRAY.includes(status) && start_time == "" && end_time == "") ||      // 休日、年休の場合は空で正常
+            (status == "午前半年休" && start_time == "14:00")    // 午前半年休の場合は14:00開始で正常
+        ) {
             return "";
         }
+        return target_date + "の開始時間が" + start_time + "です。<br/>";
 
+    }
+
+    // 勤務状況を調べる
+    function checkStatus(target_date, status) {
+        const OK_STATUS = ["勤務", "休日", "年休" , "午前半年休", "午後半年休", "コロナ休暇", "時差出勤"];
+        if (OK_STATUS.includes(status)) {
+            return "";
+        }
+        return target_date + "の勤務状況を確認してください。<br/>";
+    }
+
+    // 不足時間の調査
+    function checkLack(target_date, lack_hours) {
+        if (lack_hours == "0:00") {
+            return "";
+        }
+        return target_date + "で" + lack_hours + "の不足時間が発生しています。<br/>";
+    }
+
+    function checkRest(target_date, rest_hours, status) {
+        // 休憩時間が0でも問題ない場合
+        const OK_STATUS = ["休日", "年休" , "午前半年休", "午後半年休", "コロナ休暇"];
+        if (
+            (OK_STATUS.includes(status) && rest_hours == "0:00") ||   // 休み、または半休の場合は0:00でもOK
+            rest_hours == "1:00"    // 1時間とっていればOK
+        ) {
+            return "";
+        }
+        return target_date + "の休憩時間を確認してください。<br/>";
     }
 }
